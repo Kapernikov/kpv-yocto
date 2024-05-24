52441cfe230c9af0308e214c1ed4cd527fc24ff5
Frank Dekervel <kervel@gmail.com>
2024-05-24 17:31:13 +0200

    fix typo

--- run_musl.sh
+++ run_musl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 qemu-system-x86_64  \
 	-object rng-random,filename=/dev/urandom,id=rng0 \
-	-device virtio-rng-pci,rng=rng0 <
+	-device virtio-rng-pci,rng=rng0 \
 	-drive file=build/tmp-musl/deploy/images/qemux86-64/core-image-full-cmdline-qemux86-64.rootfs.wic \
 	-usb -device usb-tablet -usb -device usb-kbd \
 	 -cpu IvyBridge -smp 4 -enable-kvm \
