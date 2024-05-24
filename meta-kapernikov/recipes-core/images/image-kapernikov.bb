fb171bcc74b9630f4f99d42d50410645fe42139d
Frank Dekervel <kervel@gmail.com>
2024-05-24 18:03:39 +0200

    swupdate layer

---
+++ meta-kapernikov/recipes-core/images/image-kapernikov.bb
@@ -0,0 +1,11 @@
+SUMMARY = "Kapernikov image"
+
+IMAGE_FEATURES += "splash ssh-server-openssh package-management"
+
+IMAGE_INSTALL = "\
+    packagegroup-core-boot \
+    packagegroup-core-full-cmdline \
+    ${CORE_IMAGE_EXTRA_INSTALL} \
+    "
+
+inherit core-image
--- run_musl.sh
+++ run_musl.sh
@@ -2,7 +2,7 @@
 qemu-system-x86_64  \
 	-object rng-random,filename=/dev/urandom,id=rng0 \
 	-device virtio-rng-pci,rng=rng0 \
-	-drive file=build/tmp-musl/deploy/images/qemux86-64/core-image-full-cmdline-qemux86-64.rootfs.wic \
+	-drive file=build/tmp-musl/deploy/images/qemux86-64/image-kapernikov-qemux86-64.rootfs.wic \
 	-usb -device usb-tablet -usb -device usb-kbd \
 	 -cpu IvyBridge -smp 4 -enable-kvm \
 	 -m 256 -serial mon:vc -serial null -device virtio-vga  -display gtk,show-cursor=on
