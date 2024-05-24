5d82760a1db51997eefbd6db3207fc2778cba4db
Frank Dekervel <kervel@gmail.com>
2024-05-24 17:30:24 +0200

    switch to WIC for swupdate

--- run_musl.sh
+++ run_musl.sh
@@ -1,3 +1,9 @@
-qemu-system-x86_64 -object rng-random,filename=/dev/urandom,id=rng0 -device virtio-rng-pci,rng=rng0 \
-	 -drive file=/home/kervel/projects/yocto/kas/build/tmp-musl/deploy/images/qemux86-64/core-image-full-cmdline-qemux86-64.rootfs.ext4,if=virtio,format=raw -usb -device usb-tablet -usb -device usb-kbd   -cpu IvyBridge -smp 4 -enable-kvm -m 256 -serial mon:vc -serial null -device virtio-vga  -display gtk,show-cursor=on \
-	 -kernel /home/kervel/projects/yocto/kas/build/tmp-musl/deploy/images/qemux86-64/bzImage -append 'root=/dev/vda rw'
+#!/bin/bash
+qemu-system-x86_64  \
+	-object rng-random,filename=/dev/urandom,id=rng0 \
+	-device virtio-rng-pci,rng=rng0 <
+	-drive file=build/tmp-musl/deploy/images/qemux86-64/core-image-full-cmdline-qemux86-64.rootfs.wic \
+	-usb -device usb-tablet -usb -device usb-kbd \
+	 -cpu IvyBridge -smp 4 -enable-kvm \
+	 -m 256 -serial mon:vc -serial null -device virtio-vga  -display gtk,show-cursor=on
+
